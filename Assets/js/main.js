af839e0689ff85ecd357df06a4b78db5af47d331
pat8308vinod <41480115+pat8308vinod@users.noreply.github.com>
2023-09-17 14:19:34 +0530

    first commit1

---
+++ Assets/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $(".navbar-toggler").click(function () {
+      $("#span-1").toggleClass("navbar-toggler-icon");
+      $("#span-1").addClass("btn-close");
+    });
+  });
